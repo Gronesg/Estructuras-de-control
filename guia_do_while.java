50fda1ce26f2e559e1d41ab589e99999420388fa
Jose Sgoifo <josesgsg@gmail.com>
2023-03-13 19:21:51 -0300

    save2

--- guia_do_while.java
+++ guia_do_while.java
@@ -14,25 +14,30 @@ public class guia_do_while {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("///////////////////");
-        int numero;
-        int cant_numeros = 1;
-        int suma = 0;
 
+        //Inicializamos las variables
+        int numero;             //numeros a ingresar
+        int cant_numeros = 1;         //contador de numeros ingresados
+        int suma = 0;           //aloja la suma total
 
+        //Inicio de ciclo do while
         do {
-            System.out.print("Ingrese el numero a sumar: ");
+            System.out.print("Ingrese el numero a sumar: "); //Se pide ingresar en cada iteracion al usuario
             numero = entrada.nextInt();
-            if (numero > 0){
-                suma = suma + numero;
-            }
-            if (numero == 0){
+            //Control de numero positivo
+            if (numero > 0){                          //Si el numero es positivo
+                suma = suma + numero;                 //se va a acumular en la variable suma
+
+            /*Control de condicion para salir del ciclo do while
+            En este caso, que el numero ingresado sea 0*/
+            } else if (numero == 0){
                 System.out.println("Se capturó el numero 0");
-                break;
+                break;                                 //Comando para salir del ciclo
             }            
-            cant_numeros++;
+            cant_numeros++;                            //Incremento de contador de numeros ingresados
         } while (cant_numeros < 20);
 
-        System.out.println("La suma de los numeros ingresados es de: " + suma);
+        System.out.println("La suma de los numeros ingresados es de: " + suma);     //Muestra la suma total de todos los numeros ingresados
         entrada.close();
     }
 }
