e607b8127ccd3c16516da915aaac762c30b5a11b
Jose Sgoifo <josesgsg@gmail.com>
2023-03-14 17:56:22 -0300

    save2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
+/*
+ * Crear un programa que dado un número determine si es par o impar.
+ */
+
 public class Ejercicio1 {
     
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("///////////////////////");
+        int numero;
+        System.out.print("Ingrese un numero: ");
+        numero = entrada.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("El numero " + numero + " es par");
+        } else {
+            System.out.println("El numero " + numero + " es impar");
+        }
+        entrada.close();
+    }
 }
